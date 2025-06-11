e4d10ae7831b6716a81e9261bba0ad01a6882814
Matheus6057 <crvalomatheus@gmail.com>
2025-06-11 13:56:44 -0300

    Add files via upload

---
+++ gato/js/main.js
@@ -0,0 +1,13 @@
+const imgGato = document.getElementById('imgGato')
+const gatoDanca = document.getElementById('gatoDanca')
+const gatoSenta = document.getElementById('gatoSenta')
+
+gatoDanca.addEventListener('click', ()=>{
+    debugger;
+    imgGato.src = "./img/tenor.gif";
+    
+})
+gatoSenta.addEventListener('click', ()=>{
+    debugger;
+    imgGato.src = "./img/adoravel-gatinho-domestico-com-espaco-de-copia_23-2149167112.avif";
+})
